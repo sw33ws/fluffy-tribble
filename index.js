fb90952d2c22b07add86cd904363d20f2280e3f9
Seb <gillisl22@outlook.com>
2022-06-09 20:30:34 -0400

    changes so the file prints, just not the right kind

---
+++ index.js
@@ -0,0 +1,49 @@
+const inquirer = require('inquirer');
+
+var fs = require('fs');
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      message: 'Write your Title here!',
+      name: 'title',
+    },
+    {
+      type: 'input',
+      message: 'Write your github username',
+      name: 'username',
+    },
+    {
+        type: 'input',
+        message: 'Write your Description here!',
+        name: 'description',
+      },
+      {
+        type: 'input',
+        message: 'Write how to use the program here!',
+        name: 'usage',
+      },
+      {
+        type: 'input',
+        message: 'Write your installation guide here!',
+        name: 'installation',
+      },
+      {
+        type: 'input',
+        message: 'Write your credited source here!',
+        name: 'credit',
+      },
+      {
+        type: 'input',
+        message: 'Write your email here!',
+        name: 'email',
+      },
+  ])
+  .then((data) => {
+    const filename = `${data.title.toLowerCase().split(' ').join('')}.md`;
+    fs.writeFile(filename, MD.stringify(data, null, '\t'), (err) =>
+      err ? console.log(err) : console.log('Success!')
+    );
+  })
+  
